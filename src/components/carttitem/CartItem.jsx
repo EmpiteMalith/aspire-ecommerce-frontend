72ec14894eddd5b57047c7fd6e140cf3dd132a9b
empitemalith <malith@empite.com>
2023-02-15 20:45:44 +0530

    Cart Changes.

--- src/components/carttitem/CartItem.jsx
+++ src/components/carttitem/CartItem.jsx
@@ -11,18 +11,20 @@ import {
   ImageContainer,
   ItemQuantity,
   ItemContainer,
-  RemoveItem
+  RemoveItem,
 } from "../carttitem/CartItemStyles";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 
 const CartItem = () => {
   return (
-    <ItemContainer>
+    <ItemContainer className="cart-item-container">
       <Item>
         <ImageContainer>
-          <RemoveItem><CloseIcon sx={{ fontSize: 14 }}/></RemoveItem>
+          <RemoveItem>
+            <CloseIcon sx={{ fontSize: 14 }} />
+          </RemoveItem>
           <Image
             src="https://m.media-amazon.com/images/I/61GJQyjIIhL._AC_UY218_.jpg"
             alt="item"
@@ -44,14 +46,18 @@ const CartItem = () => {
       </Item>
       <Item>
         <ImageContainer>
-        <RemoveItem><CloseIcon sx={{ fontSize: 14 }}/></RemoveItem>
+          <RemoveItem>
+            <CloseIcon sx={{ fontSize: 14 }} />
+          </RemoveItem>
           <Image
             src="https://m.media-amazon.com/images/I/61IG46p-yHL._AC_UY218_.jpg"
             alt="item"
           />
         </ImageContainer>
         <ItemDetails>
-          <ItemName>DualShock 4 Wireless Controller for PlayStation 4 - Jet Black</ItemName>
+          <ItemName>
+            DualShock 4 Wireless Controller for PlayStation 4 - Jet Black
+          </ItemName>
           <ItemPrice>$ 58.85</ItemPrice>
         </ItemDetails>
         <ItemPriceContainer>
@@ -66,7 +72,83 @@ const CartItem = () => {
       </Item>
       <Item>
         <ImageContainer>
-        <RemoveItem><CloseIcon sx={{ fontSize: 14 }}/></RemoveItem>
+          <RemoveItem>
+            <CloseIcon sx={{ fontSize: 14 }} />
+          </RemoveItem>
+          <Image
+            src="https://m.media-amazon.com/images/I/81xKdwCO2hL._AC_UL320_.jpg"
+            alt="item"
+          />
+        </ImageContainer>
+        <ItemDetails>
+          <ItemName>HP Victus 15 Gaming Laptop</ItemName>
+          <ItemPrice>$ 688.88</ItemPrice>
+        </ItemDetails>
+        <ItemPriceContainer>
+          <QtyInc>
+            <AddIcon sx={{ fontSize: 20 }} />
+          </QtyInc>
+          <ItemQuantity>4</ItemQuantity>
+          <QtyDec>
+            <RemoveIcon sx={{ fontSize: 20 }} />
+          </QtyDec>
+        </ItemPriceContainer>
+      </Item>
+      <Item>
+        <ImageContainer>
+          <RemoveItem>
+            <CloseIcon sx={{ fontSize: 14 }} />
+          </RemoveItem>
+          <Image
+            src="https://m.media-amazon.com/images/I/61GJQyjIIhL._AC_UY218_.jpg"
+            alt="item"
+          />
+        </ImageContainer>
+        <ItemDetails>
+          <ItemName>BINNUNE Wireless Gaming Headset</ItemName>
+          <ItemPrice>$ 39.99</ItemPrice>
+        </ItemDetails>
+        <ItemPriceContainer>
+          <QtyInc>
+            <AddIcon sx={{ fontSize: 20 }} />
+          </QtyInc>
+          <ItemQuantity>5</ItemQuantity>
+          <QtyDec>
+            <RemoveIcon sx={{ fontSize: 20 }} />
+          </QtyDec>
+        </ItemPriceContainer>
+      </Item>
+      <Item>
+        <ImageContainer>
+          <RemoveItem>
+            <CloseIcon sx={{ fontSize: 14 }} />
+          </RemoveItem>
+          <Image
+            src="https://m.media-amazon.com/images/I/61IG46p-yHL._AC_UY218_.jpg"
+            alt="item"
+          />
+        </ImageContainer>
+        <ItemDetails>
+          <ItemName>
+            DualShock 4 Wireless Controller for PlayStation 4 - Jet Black
+          </ItemName>
+          <ItemPrice>$ 58.85</ItemPrice>
+        </ItemDetails>
+        <ItemPriceContainer>
+          <QtyInc>
+            <AddIcon sx={{ fontSize: 20 }} />
+          </QtyInc>
+          <ItemQuantity>15</ItemQuantity>
+          <QtyDec>
+            <RemoveIcon sx={{ fontSize: 20 }} />
+          </QtyDec>
+        </ItemPriceContainer>
+      </Item>
+      <Item>
+        <ImageContainer>
+          <RemoveItem>
+            <CloseIcon sx={{ fontSize: 14 }} />
+          </RemoveItem>
           <Image
             src="https://m.media-amazon.com/images/I/81xKdwCO2hL._AC_UL320_.jpg"
             alt="item"
